bf8a8d399a2d2d0f8f54de44597ad547c8ecb3ef
Aditya Padhye <padhyeav@gmail.com>
2014-04-27 15:58:06 +0930

    First Commit

---
+++ src/runtimeclasstest/RuntimeClassTest.java
@@ -0,0 +1,64 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package runtimeclasstest;
+
+import java.io.*;
+
+/**
+ *
+ * @author Adix
+ */
+public class RuntimeClassTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+
+        // TODO code application logic here
+        String line;
+        OutputStream stdin = null;
+        InputStream stderr = null;
+        InputStream stdout = null;
+
+        // launch EXE and grab stdin/stdout and stderr
+        Process process2 = Runtime.getRuntime().exec("javac /Users/Adix/Desktop/Test.java");
+        Process process = Runtime.getRuntime().exec("java /Users/Adix/Desktop/Test");
+        stdin = process.getOutputStream();
+        stderr = process.getErrorStream();
+        stdout = process.getInputStream();
+
+        // "write" the parms into stdin
+        /**line = "param1" + "\n";
+        stdin.write(line.getBytes());
+        stdin.flush();
+
+        line = "param2" + "\n";
+        stdin.write(line.getBytes());
+        stdin.flush();
+
+        line = "param3" + "\n";
+        stdin.write(line.getBytes());
+        stdin.flush();
+
+        stdin.close();**/
+
+        // clean up if any output in stdout
+        BufferedReader brCleanUp =
+                new BufferedReader(new InputStreamReader(stdout));
+        while ((line = brCleanUp.readLine()) != null) {
+            System.out.println ("[Stdout] " + line);
+        }
+        brCleanUp.close();
+
+        // clean up if any output in stderr
+        brCleanUp =
+                new BufferedReader(new InputStreamReader(stderr));
+        while ((line = brCleanUp.readLine()) != null) {
+            System.out.println ("[Stderr] " + line);
+        }
+        brCleanUp.close();
+    }
+}
